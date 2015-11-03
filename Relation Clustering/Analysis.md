589d5724f3fa4be9289343cbcfaf96a980c4024d
nickmarton <nickmarton1993@gmail.com>
2015-11-03 16:20:00 -0500

    adding word2vec relation clustering

---
+++ Relation Clustering/Analysis.md
@@ -0,0 +1,17 @@
+# Nicholas Marton
+## 661013221
+## Natural Language Processing Assignment 3
+___
+### Analysis:
+---
+During the testing my implementation, I was unable to achieve an F-score of over 0.45. After analyzing the model, I have uncovered some ideas for why the error is so high and some possible improvements.
+##### Error:
+The error in the model is extremely high; on average I obtained around a 0.39 F-score. However, the recall was, on average, significantly higher than the precision. I believe a couple key things contribute to this outcome:
+* The data set was very small relative to the more robust and computationally intensive word2vec models. With more data, pecision along with recall would increase.
+* The word2vec phrase training considered only bigrams in its phrase training period. The vast majority of the data in *triggerdata* consists of phrases with more than two words.
+* Pre-trained models did not include the majority of the data within *triggerdata* in their vocabulary.
+
+##### Possible Improvements:
+The errors in the model made it extremely hard to generalize well, but improvements on these errors can be made.
+* The use of a much bigger pre-trained model would definitely improve some of the error, however this is intractable on my machines. Then, perhaps through the use of some distributed paradigms or computing (on AWS perhaps), using a bigger pre-trained model would become tractible and would produce much better results.
+* Another improvement that can be made is to allow word2vec to consider higher-order N-grams in its phrase training period. If this were possible, a lot more of the words in *triggerdata* would be present in the vocabulary and the model would more than likely be able to generalize a lot better; this would however be more computationally intensive, but the resulting phrases could be saved to stable memory.
---
+++ Relation Clustering/w2v_cluster.py
@@ -0,0 +1,131 @@
+"""
+Nicholas Marton
+661013221
+
+Natural Language Processing Assignment 3.
+
+To build dependencies, use
+1. pip install word2vec
+2. pip install bcubed
+"""
+
+import os
+import codecs
+import bcubed
+import logging
+import word2vec
+from collections import defaultdict
+
+logging.basicConfig(
+    format='%(asctime)s:\t %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.INFO)
+
+def aggregate_input_and_ground_truths(path='./triggerdata/'):
+    """Go into ./triggerdata/ and concatenate all lists into single list."""
+    data = []
+    ldict = defaultdict(set)
+
+    for filename in os.listdir(path):
+        with open(path + filename, "r") as f:
+            for line in f:
+                entry = unicode(line.strip().replace(" ", "_"), errors='ignore')
+                entry = str(entry)
+
+                ldict[entry].add(filename[0:filename.find(".")])
+                data.append(entry)
+
+
+    with open("gt_input.txt", "w") as f:
+        for datum in data:
+            f.write(datum + '\n')
+
+    return ldict
+
+def preprocess():
+    """Preprocess given data and build vocabulary file."""
+
+    def extend_vocab():
+        """."""
+        ground_truth_vocab = ''
+        with open('gt_input.txt', 'r') as f:
+            for line in f:
+                ground_truth_vocab += line.strip()
+                ground_truth_vocab += " "
+        ground_truth_vocab = ground_truth_vocab[:-1]
+
+        text8_vocab = ''
+        with open('./text8-phrases', 'r') as f:
+            for line in f:
+                text8_vocab += line
+
+        return text8_vocab + " " + ground_truth_vocab
+
+    logging.info('Beginning preprocessing:')
+
+    #covert text8 vocabulary to include bigram phrases
+    word2vec.word2phrase('./text8', './text8-phrases', verbose=True, min_count=1)
+
+    logging.info('Done creating test8-phrases.')
+
+    #extend text8-phrases vocab with ground truth vocab then write to file
+    full_vocab = extend_vocab()
+    with open ('./text8-phrases-extra', 'w') as f:
+        f.write(full_vocab)
+
+    logging.info('Done creating test8-phrases-extra')
+    logging.info('Done preprocessing')
+
+def main():
+    """Main method."""
+    k = 35
+
+    #write ground truth vocabulary to gt_input.txt and get ground truth
+    #dictionary
+    ldict = aggregate_input_and_ground_truths()
+    logging.info('Done generating ldict and ground truth text file.')
+
+    #if file containing clusters hasn't already been created, create it
+    if not os.path.isfile('./clusters.txt'):
+
+        preprocess()
+
+        #train word2vec and cluster output from the full vocab
+        word2vec.word2clusters('./text8-phrases-extra', './clusters.txt',
+                                            k, verbose=True, min_count=1)
+
+        logging.info('Done training.')
+        logging.info('Done creating clusters.')
+
+    #load clusters
+    clusters = word2vec.load_clusters('./clusters.txt')
+
+    #build cluster dictionary from full vocabulary
+    cdict = {}
+    for i in range(0, k):
+        for word in clusters.get_words_on_cluster(i):
+            cdict[word] = set([i])
+
+    logging.info('Done generating cdict.')
+
+    #trim cluster dictionary down to only keys included in ground truths
+    trimmed_cdict = {}
+    for key in ldict.keys():
+        try:
+            trimmed_cdict[key] = cdict[key]
+        except:
+            pass
+
+    logging.info('done trimming cdict; begining scoring\n')
+
+    #compute bcubed score
+    precision = bcubed.precision(trimmed_cdict, ldict)
+    recall = bcubed.recall(trimmed_cdict, ldict)
+    fscore = bcubed.fscore(precision, recall)
+
+    print "precision: {p}, \t recall: {r}, \t fscore: {f}".format(p=precision, r=recall, f=fscore)
+
+    logging.info('done scoring\n')
+
+if __name__ == "__main__":
+    main()
