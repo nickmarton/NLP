48797581807748862016168c0e5c7f6753f21942
nickmarton <nickmarton1993@gmail.com>
2015-11-04 00:33:25 -0500

    refactor

--- DeepNER/Preprocess.py
+++ DeepNER/Preprocess.py
@@ -10,58 +10,81 @@ logging.basicConfig(
     datefmt='%m/%d/%Y %I:%M:%S %p',
     level=logging.INFO)
 
-def concatenate(root="./Data/", output_file='./data.csv'):
+def get_data(filename='./data.csv'):
     """
-    Recursively scan directory and concatenate files into a single csv file if
-    output_file provided, otherwise, return data as a pandas DataFrame object.
+    Try to read data from filename. 
     
-    Function assumes values in files are separated by tabs and only 2 relevant
-    columns.
+    If reading fails create csv file with that filename from private data
+    and then read it.
     """
 
-    #type checking
-    if not isinstance(output_file, str) and output_file != None:
-        raise TypeError("output_file parameter must be a string or None")
+    def make_csv(root="./Data/", output_file=filename):
+        """
+        Recursively scan directory and concatenate files into a single csv file if
+        output_file provided.
+        
+        Function assumes values in files are separated by tabs and only 2 relevant
+        columns.
+        """
 
-    data = []
+        #type checking
+        if not isinstance(output_file, str) and output_file != None:
+            raise TypeError("output_file parameter must be a string or None")
 
-    #use counter to log every 10000 words
-    counter = 0
-    bad_lines = 0
-    #walk from root provided getting every file along the way
-    for path, subdirs, files in os.walk(root):
-        for name in files:
-            with codecs.open(os.path.join(path, name), 'r', encoding='utf-8',
-                                                        errors='ignore') as f:
-                #try to append each word tag pair if possible
-                for line in f:
-                    try:
-                        counter += 1
-                        word, tag = line.strip('\n').split('\t')[0:2]
-                        data.append([word, tag])
-                    except ValueError:
-                        bad_lines += 1
-                    
-                    #write to log every 1000 words
-                    if counter % 10000 == 0:
-                        logging.info(str(counter) + " words processed.")
+        data = []
 
-    logging.info("TOTAL AMOUNT OF MALFORMED LINES: " + str(bad_lines))
+        #use counter to log every 10000 words
+        counter = 0
+        bad_lines = 0
+        good_lines = 0
+        line_count = 0
+        #walk from root provided getting every file along the way
+        for path, subdirs, files in os.walk(root):
+            for name in files:
+                with codecs.open(os.path.join(path, name), 'r', encoding='utf-8',
+                                                            errors='ignore') as f:
+                    #try to append each word tag pair if possible
+                    for line in f:
+                        try:
+                            counter += 1
+                            word, tag = line.strip('\n').split('\t')[0:2]
+                            good_lines += 1
+                            data.append([word, tag])
+                        except ValueError:
+                            bad_lines += 1
+                        finally:
+                            line_count += 1
+                        
+                        #write to log every 1000 words
+                        if counter % 10000 == 0:
+                            logging.info(str(counter) + " words processed.")
 
-    #make DataFrame object from data string
-    pd_data = pd.DataFrame(data, columns=['word', 'tag'])
+        logging.info("TOTAL AMOUNT OF MALFORMED LINES: " + str(bad_lines))
+        logging.info("TOTAL AMOUNT OF GOOD LINES: " + str(good_lines))
+        logging.info("TOTAL LINES: " + str(line_count))
 
-    #if output file provided, write csv, otherwise return DataFrame
-    if output_file:
+        #make DataFrame object from data string
+        pd_data = pd.DataFrame(data, columns=['word', 'tag'])
+
+        #if output file provided, write csv, otherwise return DataFrame
         pd_data.to_csv(output_file, encoding='utf-8')
+
+    def load_csv(input_file=filename):
+        """Load csv data file into pandas DataFrame object."""
+        return pd.read_csv(input_file)
+
+    #if file with filename exists
+    if os.path.exists(filename):
+        return load_csv(input_file=filename)
     else:
-        return pd_data
+        make_csv(output_file=filename)
+        return load_csv(input_file=filename)
+
 
 def main():
     """."""
-    concatenate()
-    #print data[0]
-    #print pd.DataFrame(data, columns=['word', 'tag', 'offset1', 'offset2'])
+    df = get_data()
+    print df[0:5]
 
 if __name__ == "__main__":
     main()
